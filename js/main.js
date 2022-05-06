464523996e9924afbc399a0ae432d0af27bf2e59
Kevin Lahoda <klahoda@binghamton.edu>
2022-05-06 06:35:30 -0400

    text fade in and structural fix

--- js/main.js
+++ js/main.js
@@ -6,11 +6,21 @@ $(document).scroll(function () {
     } else {
         $('#part-1').fadeOut();
     }
+    if (y > ( $(window).height() * 1 ) && y < ( $(window).height() * 2 )  ) {
+        $('#part-1').find('h2, p').fadeIn();
+    } else {
+        $('#part-1').find('h2, p').fadeOut();
+    }
 
     if (y > ( $(window).height() * 2 ) ) {
         $('#part-2').fadeIn();
     } else {
         $('#part-2').fadeOut();
+    }
+    if (y > ( $(window).height() * 2 ) && y < ( $(window).height() * 3 )  ) {
+        $('#part-2').find('h2, p').fadeIn();
+    } else {
+        $('#part-2').find('h2, p').fadeOut();
     }
 
     if (y > ( $(window).height() * 3 ) ) {
@@ -18,11 +28,21 @@ $(document).scroll(function () {
     } else {
         $('#part-3').fadeOut();
     }
+    if (y > ( $(window).height() * 3 ) && y < ( $(window).height() * 4 )  ) {
+        $('#part-3').find('h2, p').fadeIn();
+    } else {
+        $('#part-3').find('h2, p').fadeOut();
+    }
 
     if (y > ( $(window).height() * 4 ) ) {
         $('#part-4').fadeIn();
     } else {
         $('#part-4').fadeOut();
+    }
+    if (y > ( $(window).height() * 4 ) && y < ( $(window).height() * 5 )  ) {
+        $('#part-4').find('h2, p').fadeIn();
+    } else {
+        $('#part-4').find('h2, p').fadeOut();
     }
 
     if (y > ( $(window).height() * 5 ) ) {
@@ -30,6 +50,12 @@ $(document).scroll(function () {
     } else {
         $('#part-5').fadeOut();
     }
+    if (y > ( $(window).height() * 5 ) ) {
+        $('#part-5').find('h2, p').fadeIn();
+    } else {
+        $('#part-5').find('h2, p').fadeOut();
+    }
+
 
 
 });
